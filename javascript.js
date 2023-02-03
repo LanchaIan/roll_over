03ebc0138cd45aaaee01247ae63ac7e3122df21d
Ian Santana <thief.jones@gmail.com>
2023-02-03 13:31:55 +0000

    terminado

---
+++ javascript.js
@@ -0,0 +1,19 @@
+function entra1(img){
+    document.getElementById(img).src = "imagenes/prim-indir.gif";
+}
+
+function sale1(img){
+    document.getElementById(img).src = "imagenes/prim-dir.gif";
+}
+
+function entra2(img){
+    document.getElementById(img).src = "imagenes/secun-indir.gif";
+}
+
+function sale2(img){
+    document.getElementById(img).src = "imagenes/secun-dir.gif";
+}
+
+function cerrar() {
+    window.close();
+}
